012f67168cf6d48cd4901d1d788448f4d8511e72
unknown <golbery.santos@yahoo.com.br>
2024-09-23 17:51:36 -0400

    pacotes para calculadora

--- src/main/java/br/com/helper/calculadora/Calculadora.java
+++ src/main/java/br/com/helper/calculadora/Calculadora.java
@@ -1,5 +1,13 @@
 package br.com.helper.calculadora;
 
 public class Calculadora {
+	private void calcular() {
+		somar();
 
+	}
+
+	private void somar() {
+		// TODO Auto-generated method stub
+		
+	}
 }
