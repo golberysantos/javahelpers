136c2cf921d13e23549ea607d6459ec673f931ce
golberysantos <golbery.santos@yahoo.com.br>
2024-09-30 21:24:02 -0400

    calc

---
+++ src/main/java/br/com/helper/desktop/TelaDesktop.java
@@ -0,0 +1,29 @@
+package br.com.helper.desktop;
+
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class TelaDesktop extends JFrame{
+	
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	public void exibir() {
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		
+		JLabel lblNewLabel = new JLabel("calculadora");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		contentPane.add(lblNewLabel);
+		setVisible(true);
+	}
+}
