81491ca85bb4d88696602c1b76fc72c2576609b3
golberysantos <golbery.santos@yahoo.com.br>
2025-02-06 21:48:16 -0400

    ---

--- src/main/resources/templates/gvrer-2023.2.190/static/js/login.js
+++ src/main/resources/templates/gvrer-2023.2.190/static/js/login.js
@@ -1,4 +1,4 @@
-const usuarioBd = "gabriel@gmail";
+/*const usuarioBd = "gabriel@gmail";
 const senhaBd = "123";
 var usuario;
 var senha;
@@ -18,4 +18,17 @@ function verificar() {
         alert("Erro!! Usuário ou senha incorreto.");
         console.log("Erro!! Usuário ou senha incorreto.");
     }
-}
+}*/
+// Código para alternar a visibilidade da senha
+const passwordField = document.getElementById('senha');
+const togglePasswordIcon = document.getElementById('togglePassword');
+
+togglePasswordIcon.addEventListener('click', function () {
+  // Alterna o tipo da senha entre 'password' e 'text'
+  const type = passwordField.type === 'password' ? 'text' : 'password';
+  passwordField.type = type;
+
+  // Alterna o ícone entre 'fa-eye' (olho aberto) e 'fa-eye-slash' (olho fechado)
+  this.classList.toggle('fa-eye');
+  this.classList.toggle('fa-eye-slash');
+});
