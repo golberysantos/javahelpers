c04b89bf9e1c8a437c1d6e8346798353ee611c1b
golberysantos <golbery.santos@yahoo.com.br>
2025-02-10 21:38:25 -0400

    novas telas

---
+++ src/main/resources/templates/gvrer-2023.2.190/static/js/redirecionarParaOutraPagina.js
@@ -0,0 +1,30 @@
+
+function abrirEmOutraAba(url) {
+    let e = document.querySelector("#button");
+    e.addEventListener(
+        "click",
+        function (e) {
+            e.preventDefault();
+            abrirAba(url)
+        }
+    );
+}
+
+function abrirAba(url) {
+    window.location.assign(url); // carrega link na janela atual
+}
+
+function name(params) {
+    setTimeout(function () {
+        window.location.href = "http://www.google.com";
+        //ou
+        window.location.assign(url); // carrega link na janela atual
+        //ou
+        window.open(url); // abre link em outra janela
+    }, 5000);
+}
+
+
+
+
+
