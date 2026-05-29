654db545bf722d0aa808420d8987c9169b71eb54
Golbery <golbery.santos@yahoo.com.br>
2026-05-29 19:47:18 -0400

    testes

--- src/main/java/br/com/helper/principal/PrincipalOperador.java
+++ src/main/java/br/com/helper/principal/PrincipalOperador.java
@@ -1,5 +1,9 @@
 package br.com.helper.principal;
 
+import java.util.Scanner;
+
+import com.sun.org.apache.bcel.internal.generic.SWITCH;
+
 import br.com.helper.operadores.OperadorIncremento;
 
 public class PrincipalOperador {
@@ -7,7 +11,21 @@ public class PrincipalOperador {
 	public static void main(String[] args) {
 	System.out.println("OPERADORES");
 	OperadorIncremento oi = new OperadorIncremento();
-	oi.incremento();
+	
+	System.out.println("1. TESTE COM OPERADOR DE INCREMENTO");
+	
+	Scanner ler = new Scanner(System.in);
+	
+	switch (key) {
+	case 1: {
+
+		yield type;
+		oi.incremento();
+	}
+	default:
+		throw new IllegalArgumentException("Unexpected value: " + key);
+	}
+	
 	System.exit(0);
 
 	}
