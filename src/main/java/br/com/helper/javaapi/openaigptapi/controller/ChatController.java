c333e2157463edccc6dda3a16cc763865850c8a9
Golbery <golbery.santos@yahoo.com.br>
2026-07-09 00:24:44 -0400

    feat: ajustes em consumo de api chatgpt

--- src/main/java/br/com/helper/javaapi/openaigptapi/controller/ChatController.java
+++ src/main/java/br/com/helper/javaapi/openaigptapi/controller/ChatController.java
@@ -1,5 +1,32 @@
 package br.com.helper.javaapi.openaigptapi.controller;
 
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.helper.javaapi.openaigptapi.dto.PerguntaDTO;
+import br.com.helper.javaapi.openaigptapi.dto.RespostaDTO;
+import br.com.helper.javaapi.openaigptapi.service.ChatGPTService;
+
+
+@RestController
+@RequestMapping("/chat")
 public class ChatController {
 
+    private final ChatGPTService service;
+
+    public ChatController(ChatGPTService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public RespostaDTO perguntar(@RequestBody PerguntaDTO pergunta) {
+
+        String resposta = service.perguntar(
+                pergunta.getPergunta());
+
+        return new RespostaDTO(resposta);
+    }
 }
--- src/main/java/br/com/helper/javaapi/openaigptapi/service/ChatGPTService.java
+++ src/main/java/br/com/helper/javaapi/openaigptapi/service/ChatGPTService.java
@@ -2,7 +2,20 @@ package br.com.helper.javaapi.openaigptapi.service;
 
 import org.springframework.stereotype.Service;
 
+import com.openai.client.OpenAIClient;
+
 @Service
 public class ChatGPTService {
 
+    private final OpenAIClient client;
+
+    public ChatGPTService(OpenAIClient client) {
+        this.client = client;
+    }
+
+    public String perguntar(String pergunta) {
+
+        // Implementaremos a chamada para a OpenAI aqui
+        return "Recebi sua pergunta: " + pergunta;
+    }
 }
