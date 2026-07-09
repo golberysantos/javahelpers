919cfd63a52d95cf273b4def96316b429ca3fda3
Golbery <golbery.santos@yahoo.com.br>
2026-07-08 22:50:37 -0400

    feat: ajustes em operadores

--- src/main/java/br/com/helper/principal/PrincipalOperador.java
+++ src/main/java/br/com/helper/principal/PrincipalOperador.java
@@ -2,31 +2,26 @@ package br.com.helper.principal;
 
 import java.util.Scanner;
 
-import com.sun.org.apache.bcel.internal.generic.SWITCH;
-
 import br.com.helper.operadores.OperadorIncremento;
 
 public class PrincipalOperador {
 
 	public static void main(String[] args) {
-	System.out.println("OPERADORES");
-	OperadorIncremento oi = new OperadorIncremento();
+		System.out.println("OPERADORES");
+		OperadorIncremento oi = new OperadorIncremento();
 
-	System.out.println("1. TESTE COM OPERADOR DE INCREMENTO");
+		System.out.println("1. TESTE COM OPERADOR DE INCREMENTO");
 
-	Scanner ler = new Scanner(System.in);
-	int key = ler.nextInt();
-	switch (key) {
-	case 1: {
+		try (Scanner ler = new Scanner(System.in)) {
+			int key = ler.nextInt();
+			switch (key) {
+			case 1:
+				oi.incremento();
 
-		yield type;
-		oi.incremento();
-	}
-	default:
-		throw new IllegalArgumentException("Unexpected value: " + key);
-	}
-
-	System.exit(0);
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + key);
+			}
+		}
 
 	}
 
