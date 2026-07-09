3355e0a15f4bc64e5fa21ee536dcb7b1e9805c7f
Golbery <golbery.santos@yahoo.com.br>
2026-07-09 00:19:54 -0400

    feat: ajustes em consumo de api chatgpt

--- src/main/java/br/com/helper/javaapi/openaigptapi/dto/RespostaDTO.java
+++ src/main/java/br/com/helper/javaapi/openaigptapi/dto/RespostaDTO.java
@@ -2,4 +2,20 @@ package br.com.helper.javaapi.openaigptapi.dto;
 
 public class RespostaDTO {
 
+	private String resposta;
+
+	public RespostaDTO() {
+	}
+
+	public RespostaDTO(String resposta) {
+		this.resposta = resposta;
+	}
+
+	public String getResposta() {
+		return resposta;
+	}
+
+	public void setResposta(String resposta) {
+		this.resposta = resposta;
+	}
 }
