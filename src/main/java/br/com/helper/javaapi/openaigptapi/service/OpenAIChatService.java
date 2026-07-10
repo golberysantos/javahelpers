96ae48196d4e0255eb943388b44f0dd85e298378
Golbery <golbery.santos@yahoo.com.br>
2026-07-10 00:51:39 -0400

    feat: implementação de consumo api chatgpt

--- src/main/java/br/com/helper/javaapi/openaigptapi/service/OpenAIChatService.java
+++ src/main/java/br/com/helper/javaapi/openaigptapi/service/OpenAIChatService.java
@@ -1,6 +1,5 @@
 package br.com.helper.javaapi.openaigptapi.service;
 
-
 import org.springframework.stereotype.Service;
 
 import br.com.helper.javaapi.openaigptapi.exception.OpenAIException;
@@ -8,15 +7,15 @@ import br.com.helper.javaapi.openaigptapi.exception.OpenAIException;
 @Service
 public class OpenAIChatService implements ChatService {
 
-    @Override
-    public String perguntar(String pergunta) {
+	@Override
+	public String perguntar(String pergunta) {
 
-        throw new OpenAIException("""
-                A integração com a OpenAI ainda não foi habilitada.
+		throw new OpenAIException("""
+				A integração com a OpenAI ainda não foi habilitada.
 
-                Obtenha uma API Key para utilizar esta implementação.
-                """);
+				Obtenha uma API Key para utilizar esta implementação.
+				""");
 
-    }
+	}
 
 }
