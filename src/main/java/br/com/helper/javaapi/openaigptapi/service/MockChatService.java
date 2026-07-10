27b40214270081d17d9023a7dfc8ce74048c372c
Golbery <golbery.santos@yahoo.com.br>
2026-07-10 01:06:48 -0400

    feat: implementação de consumo api chatgpt

--- src/main/java/br/com/helper/javaapi/openaigptapi/service/MockChatService.java
+++ src/main/java/br/com/helper/javaapi/openaigptapi/service/MockChatService.java
@@ -4,11 +4,17 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import br.com.helper.javaapi.openaigptapi.exception.OpenAIException;
-import br.com.helper.javaapi.openaigptapi.util.PromptBuilder;
+import br.com.helper.javaapi.openaigptapi.knowledge.MockKnowledgeBase;
 
 @Service
 @Primary
 public class MockChatService implements ChatService {
+
+	private final MockKnowledgeBase knowledgeBase;
+
+	public MockChatService(MockKnowledgeBase knowledgeBase) {
+		this.knowledgeBase = knowledgeBase;
+	}
 
 	@Override
 	public String perguntar(String pergunta) {
@@ -17,41 +23,21 @@ public class MockChatService implements ChatService {
 			throw new OpenAIException("A pergunta não pode estar vazia.");
 		}
 
-		String prompt = PromptBuilder.criarPromptExplicacaoJava(pergunta);
+		String resposta = knowledgeBase.buscarResposta(pergunta);
 
-		return gerarRespostaMock(prompt);
-	}
-
-	private String gerarRespostaMock(String prompt) {
-
-		String texto = prompt.toLowerCase();
-
-		if (texto.contains("polimorfismo")) {
-			return """
-					Polimorfismo é a capacidade de um objeto
-					assumir diferentes comportamentos.
-					""";
-		}
-
-		if (texto.contains("encapsulamento")) {
-			return """
-					Encapsulamento protege os atributos da classe.
-					""";
-		}
-
-		if (texto.contains("herança")) {
-			return """
-					Herança permite reutilizar código entre classes.
-					""";
+		if (resposta != null) {
+			return resposta;
 		}
 
 		return """
-				Resposta simulada da OpenAI.
+				Esta é uma resposta simulada.
 
-				Pergunta:
+				Ainda não possuo conhecimento sobre:
 
 				%s
-				""".formatted(prompt);
+
+				Em breve a OpenAI responderá esta pergunta.
+				""".formatted(pergunta);
 
 	}
 
