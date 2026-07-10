32c13fc2ad7851a2a5efb07632548c8f5d0e2694
Golbery <golbery.santos@yahoo.com.br>
2026-07-10 14:51:07 -0400

    feat: implementação de consumo api chatgpt

--- src/main/java/br/com/helper/javaapi/openaigptapi/controller/ChatController.java
+++ src/main/java/br/com/helper/javaapi/openaigptapi/controller/ChatController.java
@@ -1,6 +1,5 @@
 package br.com.helper.javaapi.openaigptapi.controller;
 
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,28 +8,21 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.helper.javaapi.openaigptapi.dto.PerguntaDTO;
 import br.com.helper.javaapi.openaigptapi.dto.RespostaDTO;
 import br.com.helper.javaapi.openaigptapi.service.ChatService;
-import br.com.helper.javaapi.openaigptapi.service.MockChatService;
-
 
 @RestController
 @RequestMapping("/chat")
 public class ChatController {
 
-	private final ChatService service;
+    private final ChatService service;
 
-	public ChatController(ChatService service) {
-	    this.service = service;
-	}
-
-    public ChatController(MockChatService service) {
+    public ChatController(ChatService service) {
         this.service = service;
     }
 
     @PostMapping
     public RespostaDTO perguntar(@RequestBody PerguntaDTO pergunta) {
 
-        String resposta = service.perguntar(
-                pergunta.getPergunta());
+        String resposta = service.perguntar(pergunta.getPergunta());
 
         return new RespostaDTO(resposta);
     }
