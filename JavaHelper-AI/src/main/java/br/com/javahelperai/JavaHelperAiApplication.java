bc29f835a0a0864051ba77704c284fb7e0250f38
Golbery <golbery.santos@yahoo.com.br>
2026-07-11 00:25:09 -0400

    feat: implementação de consumo api chatgpt

---
+++ JavaHelper-AI/src/main/java/br/com/javahelperai/JavaHelperAiApplication.java
@@ -0,0 +1,13 @@
+package br.com.javahelperai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHelperAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHelperAiApplication.class, args);
+	}
+
+}
---
+++ JavaHelper-AI/src/test/java/br/com/javahelperai/JavaHelperAiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.javahelperai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHelperAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
