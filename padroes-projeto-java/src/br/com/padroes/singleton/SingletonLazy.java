e71b9c89703d394ba4870d1472772c33ec01eea8
Golbery <golbery.santos@yahoo.com.br>
2026-07-16 04:29:24 -0400

    feat:  separação de projetos fica apenas o projeto  spring

---
+++ padroes-projeto-java/src/br/com/padroes/singleton/SingletonLazy.java
@@ -0,0 +1,22 @@
+package br.com.padroes.singleton;
+
+/**
+ * Singleton "pregui�oso".
+ * 
+ * @author falvojr
+ */
+public class SingletonLazy {
+
+	private static SingletonLazy instancia;
+	
+	private SingletonLazy() {
+		super();
+	}
+	
+	public static SingletonLazy getInstancia() {
+		if (instancia == null) {
+			instancia = new SingletonLazy();
+		}
+		return instancia;
+	}
+}
---
+++ padroes-projeto-java/src/br/com/padroes/strategy/Comportamento.java
@@ -0,0 +1,5 @@
+package br.com.padroes.strategy;
+
+public interface Comportamento {
+	void mover();
+}
