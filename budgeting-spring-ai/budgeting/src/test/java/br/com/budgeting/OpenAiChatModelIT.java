bb13e73ff4cacaae592fd3185ad61b538f0ca5a5
Golbery <golbery.santos@yahoo.com.br>
2026-07-22 02:47:42 -0400

    chore: confirguração do  .properties

--- budgeting-spring-ai/budgeting/src/test/java/br/com/budgeting/OpenAiChatModelIT.java
+++ budgeting-spring-ai/budgeting/src/test/java/br/com/budgeting/OpenAiChatModelIT.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiChatModel.ResponseFormat;
 import org.springframework.ai.openai.OpenAiChatOptions;
-import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
