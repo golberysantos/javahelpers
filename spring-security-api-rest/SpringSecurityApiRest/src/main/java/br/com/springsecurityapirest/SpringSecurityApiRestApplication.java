d46b60719c9979ec325e2cbd8f8105c4c2a6bc10
Golbery <golbery.santos@yahoo.com.br>
2026-07-25 23:53:13 -0400

    feat: start SpringSecurityApiRest

---
+++ spring-security-api-rest/SpringSecurityApiRest/src/main/java/br/com/springsecurityapirest/SpringSecurityApiRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.springsecurityapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApiRestApplication.class, args);
+	}
+
+}
